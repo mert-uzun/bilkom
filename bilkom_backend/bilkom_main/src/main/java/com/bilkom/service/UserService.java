1109034de6785115801ef801716a284ba1cf8648
Elif Bozkurt <elfbzkrtt07@gmail.com>
2025-04-29 14:07:49 +0300

    @Service error (?) that is probably fixed on main

--- bilkom_backend/bilkom_main/src/main/java/com/bilkom/service/UserService.java
+++ bilkom_backend/bilkom_main/src/main/java/com/bilkom/service/UserService.java
@@ -8,7 +8,9 @@ import com.bilkom.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.util.List;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
     @Autowired
     private UserRepository userRepository;
